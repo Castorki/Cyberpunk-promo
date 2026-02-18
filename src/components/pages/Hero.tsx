9e5d2cbb7fa6603e2f979cf83a70e269d4b83981
Darya Kauzova <amayasuzuki12@gmail.com>
2026-02-18 19:24:58 +0300

    changed background style for hero component

--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -2,6 +2,11 @@
 export const Hero = () => {
     return (
         <section className="hero center">
+            <div className="hero__background">
+                <div className="hero__background_img" ></div>
+                <div className="hero__background_img" ></div>
+                <div className="hero__background_img" ></div>
+            </div>
             <div className="hero__info">
                 <h2 className="hero__info_text">Доступно на всех платформах</h2>
                 <button className="hero__info_button">Узнать больше</button>
