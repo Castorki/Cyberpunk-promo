59abbab117841e85bdc56cec899f2a7856d4fc96
Darya Kauzova <amayasuzuki12@gmail.com>
2026-02-19 13:37:08 +0300

    ended creating a form data submission to the server

--- src/components/pages/Game.tsx
+++ src/components/pages/Game.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, type ChangeEvent, type SubmitEvent, useRef } from "react"
 import { gameConsoles } from "../../assets"
 import type { FormData } from '../../types/types'
 
@@ -8,11 +8,33 @@ export const Game = () => {
     const [email, setEmail] = useState<string>('');
     const [file, setFile] = useState<File | null>(null);
     const [checkbox, setCheckbox] = useState<boolean>(false);
-    const [emptiField, setEmptyField] = useState<boolean>(false);
+    const [emptyField, setEmptyField] = useState<boolean>(false);
     const [wrongEmail, setWrongEmail] = useState<boolean>(false);
     const [showError, setShowError] = useState<boolean>(false);
-    const [showSucces, setShowSucces] = useState<boolean>(false);
+    const [showSuccess, setShowSucces] = useState<boolean>(false);
     const [showIsLoading, setShowIsLoading] = useState<boolean>(false);
+    const fileInputRef = useRef<HTMLInputElement>(null);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value, checked, files } = e.target;
+
+        switch (name) {
+            case 'userName':
+                setName(value);
+                break;
+            case 'userEmail':
+                setEmail(value);
+                break;
+            case 'userPicture':
+                if (files && files[0]) {
+                    setFile(files[0]);
+                }
+                break;
+            case 'userAgreement':
+                setCheckbox(checked);
+                break;
+        }
+    }
 
     const checkValability = () => {
 
@@ -26,20 +48,85 @@ export const Game = () => {
         }
 
         if (!email.includes('@')) {
+            setWrongEmail(true);
+
+            setTimeout(() => {
+                setWrongEmail(false)
+            }, 3000);
+            return -1;
+        }
+
+        if (!checkbox || !file) {
             setEmptyField(true);
 
             setTimeout(() => {
                 setEmptyField(false)
             }, 3000);
+
             return -1;
         }
 
+        return {
+            name: name,
+            email: email,
+            file: file,
+            checkbox: checkbox
+        };
     }
 
-    const handleSubmit = (e: SubmitEvent) => {
+    const handleSubmit = async (e: SubmitEvent<HTMLFormElement>) => {
         e.preventDefault();
 
+        const formData: number | FormData = checkValability();
 
+        if (typeof formData === 'number') {
+            return;
+        }
+
+        const response: boolean = true
+
+        try {
+
+            setShowIsLoading(true);
+
+            // const response: Response = await fetch('', {
+            //     method: 'POST',
+            //     body: JSON.stringify(formData),
+            //     headers: {
+            //         authorization: '',
+            //         'Content-Type': 'application/json'
+            //     }
+            // })
+
+
+            if (response) /*(response.ok)*/ {
+                setShowSucces(true);
+
+                setTimeout(() => {
+                    setShowSucces(false)
+                }, 3000);
+
+                setName('');
+                setEmail('');
+                setFile(null);
+                setCheckbox(false);
+
+                if (fileInputRef.current) {
+                    fileInputRef.current.value = ''
+                };
+
+            } else {
+                setShowError(true);
+                setTimeout(() => setShowError(false), 3000);
+                // return `Error ${response.status}: ${response.statusText || 'No status text'}`
+                return `Error: 'No status text'`
+            }
+
+        } catch (error) {
+            console.log('Ошибка: ', error);
+        } finally {
+            setShowIsLoading(false)
+        }
 
     }
 
@@ -48,12 +135,14 @@ export const Game = () => {
             <h2 className="game__title">Играй и выигрывай!</h2>
             <p className="game__article">Играй в <span className="game__article_specialColor">Cyberpunk 2077</span> и получи возможность выиграть консоль <span className="game__article_specialColor">Xbox Series X</span> или <span className="game__article_specialColor">Sony PlayStation 5!</span> Заполни форму ниже и приложи скриншот о покупке игры. Итоги розыгрыша будут подведены 1 февраля. Удачи! {';)'}</p>
             <div className="game__container">
-                <form className="game__form">
+                <form className="game__form" noValidate onSubmit={handleSubmit}>
                     <input
                         className="game__form_nameInput"
                         id="userName"
                         type="text"
                         name="userName"
+                        value={name}
+                        onChange={handleChange}
                         placeholder="Как тебя зовут?"
                         required
                     />
@@ -62,29 +151,66 @@ export const Game = () => {
                         id="userEmail"
                         type="email"
                         name="userEmail"
+                        value={email}
+                        onChange={handleChange}
                         placeholder="Твой е-mail"
                         required
                     />
                     <div className="game__form_fileInputContainer">
                         <label className="game__form_uploadPicLabel" htmlFor="userPicture">
-                            <span className="uploadPicture-text">Прикрепить скриншот</span>
-                            <span className="uploadPicture-extention">.png / .jpg / .pdf</span>
+                            {file ?
+                                <span className="uploadPicture-text">{file.name}</span>
+                                :
+                                <>
+                                    <span className="uploadPicture-text">Прикрепить скриншот</span>
+                                    <span className="uploadPicture-extention">.png / .jpg / .pdf</span>
+                                </>
+                            }
                         </label>
                         <input
+                            ref={fileInputRef}
                             className="game__form_userPictureInput"
                             id="userPicture"
                             type="file"
                             name="userPicture"
+                            onChange={handleChange}
                             accept=".png, .jpg, .pdf"
                             required
                         />
                     </div>
                     <button className="game__form_submitButton" type="submit">Отправить</button>
                     <div className="game__form_checkBox">
-                        <input className="game__form_userAgreementCheck" type="checkbox" id="userAgreement" name="userAgreement" required />
+                        <input className="game__form_userAgreementCheck" type="checkbox" id="userAgreement" name="userAgreement"
+                            onChange={handleChange} checked={checkbox}
+                            required />
                         <label className="game__form_userAgreementLabel" htmlFor="userAgreement">Согласен на обработку персональных данных</label>
                     </div>
                 </form>
+                {showIsLoading && (
+                    <div className='loading'>
+                        Данные отправляются на сервер. Это может занять какое-то время...
+                    </div>
+                )}
+                {showSuccess && (
+                    <div className="success">
+                        Заявка успешно оформлена!
+                    </div>
+                )}
+                {wrongEmail && (
+                    <div className="wrongEmail">
+                        Неккоректный адрес электроной почты!
+                    </div>
+                )}
+                {emptyField && (
+                    <div className="emptyField">
+                        Не все поля были заполнены!
+                    </div>
+                )}
+                {showError && (
+                    <div className="requestError">
+                        Что-то пошло не так. Попробуйте ещё раз позже или обратитесь в тех. поддержку.
+                    </div>
+                )}
                 <div className="game__console">
                     <img className="game__console_img" src={gameConsoles} alt="Game consoles: Xbox and PlayStation 5"></img>
                 </div>
